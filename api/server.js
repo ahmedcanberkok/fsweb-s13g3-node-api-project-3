47d2b26498264dfbbb050bb76a54d9a293ba6e77
Canberk Ok <canberk.ok@gmail.com>
2023-06-15 01:56:31 +0300

    MW Projesi

--- api/server.js
+++ api/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
-
+const usersRouter =require('./users/users-router');
+const middleware = require("./middleware/middleware");
 const server = express();
+
+server.use(express.json());
+server.use(middleware.logger);
+
+
+
+
+server.get('/', (req,res) => {
+  res.json({message :" Server up and running..."})
+}); 
+
+
 
 // ekspres'in varsayılan olarak istek gövdelerinde JSON'u ayrıştıramayacağını unutmayın
 
@@ -9,5 +22,6 @@ const server = express();
 server.get('/', (req, res) => {
   res.send(`<h2>Biraz ara yazılım yazalım!</h2>`);
 });
+server.use('/api/users',usersRouter);
 
 module.exports = server;
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,94 @@
-const express = require('express');
+const express = require("express");
+const router = express.Router();
+const middleware = require("../middleware/middleware");
+const userModel =require("./users-model");
+const postsModel =require("../posts/posts-model");
 
 // `users-model.js` ve `posts-model.js` sayfalarına ihtiyacınız var
 // ara yazılım fonksiyonları da gereklidir
 
-const router = express.Router();
 
-router.get('/', (req, res) => {
+
+router.get('/', async (req, res,next) => {
   // TÜM KULLANICILARI İÇEREN DİZİYİ DÖNDÜRÜN
+  try {
+      const allUsers = await userModel.get();
+      res.json(allUsers);
+  } catch (error) {
+      next(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', middleware.validateUserId, (req, res,next) => {
   // USER NESNESİNİ DÖNDÜRÜN
   // user id yi getirmek için bir ara yazılım gereklidir
+    try {
+      res.json(req.currentUser);
+    
+    } catch (error) {
+      next(error);
+    }
 });
 
-router.post('/', (req, res) => {
-  // YENİ OLUŞTURULAN USER NESNESİNİ DÖNDÜRÜN
-  // istek gövdesini doğrulamak için ara yazılım gereklidir.
-});
+router.post('/', middleware.validatePost, async (req, res,next) => {
+    try {
+      const insertedUser = await userModel.insert({name: req.body.name});
+      res.status(201).json(insertedUser);
+    
+    } catch (error) {
+      next(error);
+    // YENİ OLUŞTURULAN USER NESNESİNİ DÖNDÜRÜN
+    // istek gövdesini doğrulamak için ara yazılım gereklidir.
+    }});
 
-router.put('/:id', (req, res) => {
+router.put('/:id', middleware.validateUserId, middleware.validateUser, async (req, res,next) => {
+  
+  try {
+      const updatedUser = await userModel.update(req.params.id,{name: req.body.name});
+      res.json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
   // YENİ GÜNCELLENEN USER NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan ara yazılım gereklidir
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',middleware.validateUserId, async (req, res,next) => {
+    try {
+       await userModel.remove(req.params.id);
+       res.json(req.currentUser);
+    } catch (error) {
+      next(error);
+    }
+
   // SON SİLİNEN USER NESNESİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', middleware.validateUserId, async (req, res,next) => {
+  try {
+    userPost = await userModel.getUserPosts(req.params.id);
+    res.json(userPost);
+  } catch (error) {
+    next(error);
+  }
   // USER POSTLARINI İÇEREN BİR DİZİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts',middleware.validateUserId,middleware.validatePost, async (req, res,next) => {
+  try {
+      let insertedPost = await postsModel.insert({user_id: req.params.id,text:req.body.text});
+      res.status(201).json(insertedPost);
+  } catch (error) {
+    next(error);
+  }
   // YENİ OLUŞTURULAN KULLANICI NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
 });
 
 // routerı dışa aktarmayı unutmayın
+
+module.exports = router;
